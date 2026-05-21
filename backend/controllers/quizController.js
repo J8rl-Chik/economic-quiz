584962708ee1411e89122a446c6c7309f21af415
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-05-21 14:06:41 +0900

    feat: 퀴즈 서버 구현
    - 퀴즈 서버를 생성하여 퀴즈 데이터를 JSON 형식으로 제공
    - 기존의 테스트 서버 코드 삭제
    - 서버가 8080 포트에서 실행되도록 설정
    - 서버 오류 처리 로직 추가
    
    주의: 기존의 라우팅 로직이 제거됨(quizRoutes.js)

---
+++ backend/controllers/quizController.js
@@ -0,0 +1,22 @@
+import http from "node:http";
+
+import readQuizFile from "../services/readQuizFile.js";
+
+const SERVER = http.createServer(async (request, response) => {
+  const QUIZZES = await readQuizFile();
+
+  response.writeHead(200, {
+    "Content-Type": "application/json; charset=utf-8",
+  });
+  response.end(JSON.stringify(QUIZZES));
+});
+
+SERVER.listen(8080);
+
+SERVER.on("listening", () => {
+  console.log("Server is running on http://localhost:8080");
+});
+
+SERVER.on("error", (err) => {
+  console.error("Server error:", err);
+});
