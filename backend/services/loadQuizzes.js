50a8dce574adda0b6e3bcda988a2ed018d984547
J8rl_Chik <95474171+J8rl-Chik@users.noreply.github.com>
2026-06-11 19:17:01 +0900

    refactor: mongoDB를 사용하여 퀴즈 로드
    - 파일 시스템에서 JSON 파일을 읽어오는 방식에서 MongoDB Atlas를 통해 데이터베이스에서 직접 데이터를 가져오는 방식으로 변경

--- backend/services/loadQuizzes.js
+++ backend/services/loadQuizzes.js
@@ -1,26 +1,22 @@
-import fs from 'node:fs/promises';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
-/** @import { RawQuiz, Quiz } from '../types.js' */
-
-const QUIZ_FILE_PATH = './backend/data/quiz.json';
-
-/**
- * 퀴즈 데이터를 읽어와 서버 포맷으로 변환하여 반환합니다.
- * @returns {Promise<Quiz[]>}
- * */
 const loadQuizzes = async () => {
-  const quizData = await fs.readFile(QUIZ_FILE_PATH, 'utf-8');
-  /** @type {RawQuiz[]} */
-  const quizzes = JSON.parse(quizData);
+  const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true
+    }
+  });
 
-  return quizzes.map(quiz => ({
-    id: quiz['번호'],
-    type: quiz['구분'],
-    question: quiz['문제내용'],
-    choices: quiz['구분'] === 'OX퀴즈' ? ['O', 'X'] : [quiz['보기1'], quiz['보기2'], quiz['보기3'], quiz['보기4']],
-    answer: Number(quiz['정답'][0]) - 1, // 값이 "2번" 혹은 "2번(X)" 형태여서 해당 보기의 인덱스로 변환
-    explanation: quiz['해설']
-  }));
+  try {
+    const database = client.db('economic-quiz-app');
+    const quizzes = await database.collection('quizzes').find().toArray();
+
+    return quizzes;
+  } finally {
+    await client.close();
+  }
 };
 
 export default loadQuizzes;
